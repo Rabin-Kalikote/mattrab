81c2e38181f416e9796a31a6acb073d06c7cf221
Rabin Kalikote <rabinkalikote33@gmail.com>
2020-07-23 15:45:51 +0545

    Checking if Current user if following self.

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,13 +3,13 @@ class RelationshipsController < ApplicationController
 
   def create
     user = User.find(params[:followed_id])
-    current_user.follow(user)
+    current_user.follow(user) if user != current_user
     redirect_back fallback_location: user
   end
 
   def destroy
     user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
+    current_user.unfollow(user) if user != current_user
     redirect_back fallback_location: user
   end
 
